17baad11c01bee2188a790af13cb3337dd0de8a6
Evilgravi <temka.202@bk.ru>
2024-12-16 21:32:32 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(9.99>9.98 and 1000 != 1001)
+
+# 3rd program
+Tsk1 = 2*2+2
+Tsk2 = 2*(2+2)
+print(Tsk1)
+print(Tsk2)
+print(Tsk1==Tsk2)
+# Or
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+# 4th program
+print(int(float("123.456")*10)%10)
